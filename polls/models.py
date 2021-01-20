c6b745ab1de3120cd15b7d432045bd0c152af061
Alex <alex_govr@mail.ru>
2021-01-20 20:49:02 +0300

    upd: make Poll.date_start uneditable

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
 
 class Poll(models.Model):
     name = models.CharField(default='NoName', null=True, max_length=80)
-    date_start = models.DateTimeField(default=datetime.now, blank=True)
+    date_start = models.DateTimeField(default=datetime.now, blank=True, editable=False)
     date_end = models.DateTimeField(blank=True)
     description = models.CharField(default='', null=True, max_length=80)
     questions = models.ManyToManyField(Question, blank=True)
