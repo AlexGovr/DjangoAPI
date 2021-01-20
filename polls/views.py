ac15c41360106c6442635fa5622e1bb72af0adc8
Alex <alex_govr@mail.ru>
2021-01-20 20:33:34 +0300

    upd: add Poll serializer

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Poll
+from .serializers import PollSerializer
 
-# Create your views here.
+
+class PollViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows polls to be viewed or edited
+    """
+    queryset = Poll.objects.all().order_by('-date_start', ).reverse()
+    serializer_class = PollSerializer
