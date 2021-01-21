6128192a92d53105a6c8e5980f323a2afbea37b8
Alex <alex_govr@mail.ru>
2021-01-21 21:52:22 +0300

    upd: add API routes

--- drfapi/urls.py
+++ drfapi/urls.py
@@ -1,13 +1,15 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from polls.views import PollViewSet, PollEditViewSet
+from polls.views import PollViewSet, PollEditViewSet, QuestionViewSet
 
 router = routers.DefaultRouter()
 router.register(r'polls', PollViewSet)
 router.register(r'polls_edit', PollEditViewSet)
+router.register(r'questions', QuestionViewSet)
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
